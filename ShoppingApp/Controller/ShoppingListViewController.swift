ae51e109fe966ef51b2c38a6f4b2f30911ecb128
NeroXerith <astar8820@gmail.com>
2025-02-18 15:04:33 +0800

    Moved Files

--- ShoppingApp/Controller/ShoppingListViewController.swift
+++ ShoppingApp/Controller/ShoppingListViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ShoppingListViewController: UIViewController {
-
+class ShoppingListViewController: UIViewController, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var tableProductList: UITableView!
+    
+  
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return productLists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableProductList.dequeueReusableCell(withIdentifier: "ProdListViewCell", for: indexPath) as! ProdListViewCell
+        cell.prodNameLabel.text = "Test"
+        cell.prodImageView.image = nil
+        cell.prodPriceLabel.text = ""
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        productLists.append(ProductDetails(name: "Banana", imageUrl: "None", price: 1.2))
+        tableProductList.dataSource = self
+        print(productLists)
         // Do any additional setup after loading the view.
     }
 
-
-}
+    }
 
