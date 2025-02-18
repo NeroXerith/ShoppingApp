7ccdb29a2d2fc9ff4d9ccaa58f787f2245b87431
NeroXerith <astar8820@gmail.com>
2025-02-18 15:00:47 +0800

    Model Folder Added Product.swift

---
+++ ShoppingApp/Controller/ShoppingListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShoppingListViewController.swift
+//  ShoppingApp
+//
+//  Created by Biene Bryle Sanico on 2/18/25.
+//
+
+import UIKit
+
+class ShoppingListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShoppingApp/Model/Product.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+struct ProductDetails {
+    let name: String
+    let imageUrl: String
+    let price: Double
+}
