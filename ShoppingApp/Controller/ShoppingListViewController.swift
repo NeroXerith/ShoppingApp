c8eede47101cb18332a4b4a038c056292290fc77
NeroXerith <astar8820@gmail.com>
2025-02-20 18:28:51 +0800

    Refractor redundancy

--- ShoppingApp/Controller/ShoppingListViewController.swift
+++ ShoppingApp/Controller/ShoppingListViewController.swift
@@ -2,18 +2,11 @@ import UIKit
 
 class ShoppingListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     // Variables
-    var fetchData = FakeStoreAPI()
+    var fakeStoreAPI = FakeStoreAPI()
     var productLists = [ProductDetails]() {
         didSet {
-            DispatchQueue.main.async {
-                self.tableProductList.reloadData()
-                self.progressView.setProgress(1.0, animated: true)
-                
-                // Hide progress bar after a short delay
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    self.progressView.isHidden = true
-                }
-            }
+            tableProductList.reloadData()
+            hideProgressView()
         }
     }
 
@@ -34,13 +27,12 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
         fetchProducts()
     }
 
-
+    // MARK: - Progress View Setup
     func setupProgressView() {
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.translatesAutoresizingMaskIntoConstraints = false
         progressView.progressTintColor = .blue
         progressView.progress = 0.0
-        progressView.isHidden = false // Show when starting
 
         navigationController?.navigationBar.addSubview(progressView)
 
@@ -52,82 +44,82 @@ class ShoppingListViewController: UIViewController, UITableViewDataSource, UITab
         ])
     }
 
-  
+    func showProgressView(with progress: Float = 0.1) {
+        progressView.isHidden = false
+        progressView.setProgress(progress, animated: true)
+    }
+
+    func hideProgressView() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.progressView.isHidden = true
+            self.refreshControl.endRefreshing()
+        }
+    }
+
+    // MARK: - Refresh Control Setup
     func setupRefreshControl() {
         refreshControl.tintColor = .clear
         refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
         tableProductList.refreshControl = refreshControl
     }
 
-  
-    func fetchProducts() {
-        progressView.progress = 0.1
-        progressView.isHidden = false
-        
-        fetchData.fetchData { [weak self] products in
-            DispatchQueue.main.async {
-                self?.productLists = products
-                self?.progressView.setProgress(1.0, animated: true)
-                
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    self?.progressView.isHidden = true
-                    self?.refreshControl.endRefreshing()
-                }
-            }
-        }
-    }
-    
     @objc func refresh() {
-        progressView.setProgress(0.1, animated: true) // Reset progress
+        showProgressView(with: 0.1) // Reset progress
         fetchProducts()
         print("Pulled to refresh!")
     }
     
-    
-    // TableView Functions
+    // MARK: - Fetching Products Asynchronously
+    func fetchProducts() {
+        showProgressView()
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            self?.fakeStoreAPI.fetchData { products in
+                DispatchQueue.main.async {
+                    self?.productLists = products
+                    self?.progressView.setProgress(1.0, animated: true)
+                }
+            }
+        }
+    }
+
+    // MARK: - TableView Functions
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return productLists.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableProductList.dequeueReusableCell(withIdentifier: "ProdListViewCell", for: indexPath) as! ProdListViewCell
-        cell.prodNameLabel.text = productLists[indexPath.row].title
-        cell.prodPriceLabel.text = "$\(String(productLists[indexPath.row].price))"
-        
         let product = productLists[indexPath.row]
+        
+        cell.prodNameLabel.text = product.title
+        cell.prodPriceLabel.text = "$\(String(product.price))"
+        
+        // Image Handling with Cache
         let imageUrlString = product.image
-                if let cachedImage = imageCache.object(forKey: imageUrlString as NSString) {
-                    // Use cached image
-                    cell.prodImageView.image = cachedImage
-                } else {
-                    // Download image asynchronously
-                    if let imageUrl = URL(string: imageUrlString) {
-                        URLSession.shared.dataTask(with: imageUrl) { [weak self] (data, response, error) in
-                            if let data = data, let image = UIImage(data: data), error == nil {
-                                DispatchQueue.main.async {
-                                    self?.imageCache.setObject(image, forKey: imageUrlString as NSString)
-                                    cell.prodImageView.image = image
-                                }
-                            }
-                        }.resume()
+        if let cachedImage = imageCache.object(forKey: imageUrlString as NSString) {
+            cell.prodImageView.image = cachedImage
+        } else if let imageUrl = URL(string: imageUrlString) {
+            // Asynchronously load the image
+            URLSession.shared.dataTask(with: imageUrl) { [weak self] (data, response, error) in
+                if let data = data, let image = UIImage(data: data), error == nil {
+                    DispatchQueue.main.async {
+                        self?.imageCache.setObject(image, forKey: imageUrlString as NSString)
+                        cell.prodImageView.image = image
                     }
                 }
+            }.resume()
+        }
         
         return cell
     }
-    
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedProduct = productLists[indexPath.row]
         
-        // Instantiate
-        if let productDetailsVC = self.storyboard?.instantiateViewController(withIdentifier: "ProductViewController") as? ProductViewController {
-            
+        if let productDetailsVC = storyboard?.instantiateViewController(withIdentifier: "ProductViewController") as? ProductViewController {
             productDetailsVC.selectedProduct = selectedProduct
-            
             navigationController?.pushViewController(productDetailsVC, animated: true)
         }
-        
     }
 }
-
