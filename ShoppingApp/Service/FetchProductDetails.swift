c0fd467fd2ba3a58a82289358560f83625a18bba
NeroXerith <astar8820@gmail.com>
2025-03-05 22:04:48 +0800

    refactor(APIService): FetchProductDetails implements the Alamofire library

--- ShoppingApp/Service/FetchProductDetails.swift
+++ ShoppingApp/Service/FetchProductDetails.swift
@@ -5,6 +5,7 @@
 //  Created by Biene Bryle Sanico on 2/26/25.
 //
 import Foundation
+import Alamofire
 
 enum FetchError: Error {
     case networkError(String)
@@ -13,15 +14,18 @@ enum FetchError: Error {
 }
 
 class FetchProducts {
-    private let fakeStoreAPI = FakeStoreAPI()
+    private let urlString = "https://fakestoreapi.com/products"
     
     func fetchProducts(completion: @escaping (Result<[ProductDetails], FetchError>) -> Void) {
-        fakeStoreAPI.fetchData { result in
-            switch result {
-            case .success(let products):
-                completion(.success(products))
-            case .failure(let error):
-                completion(.failure(.networkError(error.localizedDescription)))
+        AF.request(urlString)
+            .validate(statusCode: 200..<300)
+            .validate(contentType: ["application/json"])
+            .responseDecodable(of: [ProductDetails].self) { response in
+                switch response.result {
+                        case .success(let products):
+                            completion(.success(products))
+                        case .failure(let error):
+                            completion(.failure(.networkError(error.localizedDescription)))
             }
         }
     }
