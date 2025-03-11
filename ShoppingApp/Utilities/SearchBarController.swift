7df4cda7eec2094b84bd376e9c188d93da74df23
NeroXerith <astar8820@gmail.com>
2025-03-11 10:02:33 +0800

    Arranged Project structure

--- ShoppingApp/Utilities/SearchBarController.swift
+++ ShoppingApp/Utilities/SearchBarController.swift
@@ -11,7 +11,7 @@
 
     class SearchBarController: NSObject, UISearchResultsUpdating {
         
-        // MARK: - Properties
+        // MARK: - Variables
         let searchController: UISearchController
         @Published private(set) var filteredProducts: [ProductDetails] = [] // Publisher
         
