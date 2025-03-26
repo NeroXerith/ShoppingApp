b677e9f81c545b70472d813a8bde2fb0c389b136
Bryle Sanico <i_bienebryle.sanico@stratpoint.com>
2025-03-26 13:38:50 +0800

    feat(MVVM): Converting to MVVM - HomeViewModel

---
+++ ShoppingApp/Modules/Home/Model/ProductModel.swift
@@ -0,0 +1,23 @@
+//
+//  ProductModel.swift
+//  ShoppingApp
+//
+//  Created by Biene Bryle Sanico on 2/18/25.
+//
+
+import Foundation
+
+struct ProductDetails: Codable {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let category: String
+    let image: String
+    let rating: Rating
+}
+
+struct Rating: Codable {
+    let rate: Double
+    let count: Int
+}
