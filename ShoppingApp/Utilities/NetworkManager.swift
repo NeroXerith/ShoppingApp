90c0fd98802ce5cce4bf1da315bacb868e9e5e7b
NeroXerith <astar8820@gmail.com>
2025-04-02 10:03:57 +0800

    feat(NetworkMonitoring): Added a network connectivity checker

---
+++ ShoppingApp/Utilities/NetworkManager.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkManager.swift
+//  ShoppingApp
+//
+//  Created by Biene Bryle Sanico on 4/2/25.
+//
+
+import Combine
+import Network
+
+class NetworkManager: ObservableObject {
+    static let shared = NetworkManager()
+    private let monitorNetwork = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitoringQueue")
+    @Published var isConnected: Bool = true
+    
+    init() {
+        monitorNetwork.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+            }
+        }
+        monitorNetwork.start(queue: queue)
+    }
+}
