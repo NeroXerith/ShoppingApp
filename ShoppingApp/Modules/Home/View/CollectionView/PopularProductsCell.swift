b4640772a596f53bba6d69735ba88fbf940c1180
Bryle Sanico <i_bienebryle.sanico@stratpoint.com>
2025-04-19 13:11:43 +0800

    layout(HomeView): Adding a new collectionview for popular products

---
+++ ShoppingApp/Modules/Home/View/CollectionView/PopularProductsCell.swift
@@ -0,0 +1,17 @@
+//
+//  PopularProductsCell.swift
+//  ShoppingApp
+//
+//  Created by Biene Bryle Sanico on 4/19/25.
+//
+
+import UIKit
+
+class PopularProductsCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
