dbd877395b966cadd727c398e477e80ed875cf51
NeroXerith <astar8820@gmail.com>
2025-04-27 18:55:05 +0800

    feat(UserProfile): UI improvements

--- ShoppingApp/Modules/Profile/View/UserProfileController.swift
+++ ShoppingApp/Modules/Profile/View/UserProfileController.swift
@@ -6,24 +6,39 @@
 //
 
 import UIKit
+import SwiftUI
 
 class UserProfileController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Apply Tab Bar appearance for the UserProfile tab
+        if let tabBarController = self.tabBarController {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithDefaultBackground()
+            appearance.backgroundEffect = UIBlurEffect(style: .regular)  // Apply Blur effect
+            appearance.backgroundColor = .clear
+            
+            tabBarController.tabBar.standardAppearance = appearance
+            tabBarController.tabBar.scrollEdgeAppearance = appearance
+        }
 
-        // Do any additional setup after loading the view.
+        // SwiftUI View
+        let swiftUIView = UserProfile()
+        let hostingController = UIHostingController(rootView: swiftUIView)
+
+        addChild(hostingController)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(hostingController.view)
+
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+
+        hostingController.didMove(toParent: self)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
